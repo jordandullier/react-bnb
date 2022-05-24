3fbd701d384770ab48acd550904f263ec7ac2c58
Jordan DULLIER <jordan.dullier29@gmail.com>
2022-05-24 22:35:19 +0200

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Hero(){
     return (<section className="hero">
-                <img src="../../images/photo-grid.png" className="photo--grid"/>
+                <img src="../../images/photo-grid.png" alt="" className="photo--grid"/>
                 <h1>Expériences en ligne</h1>
                 <p>Participez à des activités uniques. Et cela près de chez vous!
                     Alors n'attendez plus...
